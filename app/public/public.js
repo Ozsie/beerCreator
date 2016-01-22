a027835dd7b75b4a1241d8df3099dc72081bc7fe
Ozsie <Ozsie@192.168.10.69>
2016-01-22 15:40:44 +0100

    public page shows non editable beer if beer is marked public started work on json download

--- app/public/public.js
+++ app/public/public.js
@@ -19,6 +19,19 @@ angular.module('beerCreator.public', ['ngRoute', 'firebase'])
     // to take an action after the data loads, use the $loaded() promise
     obj.$loaded().then(function(data) {
         console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-        $scope.beer = data;
+        if (data.public) {
+            $scope.beer = data;
+        }
     });
+    
+    $scope.downloadJSON = function() {
+        var obj = {abv: $scope.beer.abv,
+                   color: $scope.beer.color}
+        var beer = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+        var a = document.createElement('a');
+        a.href = 'data:' + beer;
+        a.download = 'data.json';
+        a.innerHTML = 'download JSON';
+        a.click();
+    };
 }]);
