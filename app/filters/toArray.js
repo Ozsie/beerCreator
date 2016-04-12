b690a36f86331ac80132fa759cd8f77cfaa45ea0
ozsie <ozsie@192.168.10.82>
2016-04-12 22:27:49 +0200

    Started work on instructions

---
+++ app/filters/toArray.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('beerCreator.filters')
+
+.filter("toArray", function(){
+    return function(obj) {
+        var result = [];
+        angular.forEach(obj, function(val, key) {
+            result.push(val);
+        });
+        return result;
+    };
+});
